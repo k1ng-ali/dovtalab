e19181b2bc5707e0bbd723f12fd61aef9dfbc7d9
Ali <im.ali@astanaqulov.ru>
2026-04-10 18:10:28 +0500

    fix: statistic

--- src/features/mainPage/statistic/Statistics.vue
+++ src/features/mainPage/statistic/Statistics.vue
@@ -8,7 +8,7 @@ const statStore = useStats()
 const userStats = computed(() => statStore.stats)
 const attemptHistory = computed(() => statStore.attempts)
 
-const isEmpty = computed(() => !userStats.value?.accuracy_percent)
+const isEmpty = computed(() => !userStats.value?.total_questions_answered)
 
 onMounted(() => {
   try {
