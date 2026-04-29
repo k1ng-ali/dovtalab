11387ae8b98188c2368e4abb6f824ecebc3e394f
Ali <im.ali@astanaqulov.ru>
2026-04-29 15:18:02 +0500

    created leaderboard page

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,13 +2,15 @@ import {createRouter, createWebHistory} from "vue-router"
 import MainPage from "@/pages/MainPage/MainPage.vue";
 import QuizPage from "@/pages/Quizes/QuizPage.vue"
 import ProfilePage from "@/pages/ProfilePage/ProfilePage.vue"
+import LeaderPage from "@/pages/LeaderPage/LeaderPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: MainPage},
         {path: '/quiz', component: QuizPage},
-        { path: '/profile', component: ProfilePage },
+        {path: '/profile', component: ProfilePage },
+        {path: '/leaders', component: LeaderPage},
     ]
 })
 
